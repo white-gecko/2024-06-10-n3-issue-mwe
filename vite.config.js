80904a9eb91ebcb7cbe309267a7a0d5a63e007f6
Natanael Arndt <arndtn@gmail.com>
2024-06-12 22:21:21 +0200

    Remove vite-plugin-node-polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nodePolyfills(),
     vue(),
   ],
   resolve: {
